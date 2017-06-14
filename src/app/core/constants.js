4343c8f0fb2ebe0b44fe2eef98948a6b6b7bf9f2
Chithralekha <chithra@altiussystems.com>
2017-06-14 13:46:42 -0400

    Constant

--- src/app/core/constants.js
+++ src/app/core/constants.js
@@ -5,5 +5,12 @@
     angular
         .module('inspinia')
         .constant('toastr', toastr)
-        .constant('moment', moment);
+        .constant('moment', moment)
+        .constant('config', {
+        baseURL : 'http://localhost:3706/api/',
+        authURL : 'http://ec2-35-164-78-65.us-west-2.compute.amazonaws.com/MagpieIdentity/',
+        //            baseURL : 'http://localhost:3706/api/',
+        //            authURL : 'http://ec2-54-148-177-100.us-west-2.compute.amazonaws.com/MagpieIdentity/',
+        oldTroutBaseURL : 'http://localhost:9092/Landing/index?n='
+    });
 })();
