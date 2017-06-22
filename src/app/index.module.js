52a9b54f9165399e55f240cd3728095f2b6ceb6f
Chithralekha <chithra@altiussystems.com>
2017-06-22 15:39:02 -0400

    IndexModule

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('inspinia', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'ngRoute']);
+    .module('inspinia', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'ngRoute','oc.lazyLoad']);
 
 })();
