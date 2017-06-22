8f0f357c13d098af7410e5d5e2c1606e731c8b02
Chithralekha <chithra@altiussystems.com>
2017-06-22 15:42:33 -0400

    Added TaskboardRoute

--- src/app/taskboard/taskboard.route.js
+++ src/app/taskboard/taskboard.route.js
@@ -13,7 +13,18 @@
         templateUrl: "app/taskboard/taskboard.html",
         data: { pageTitle: 'Taskboard' },
         controller: 'TaskboardController',
-        controllerAs: 'vm'
+        controllerAs: 'vm',        
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            name: 'ui.sortable',
+                            files: ['../../bower_components/angular-ui-sortable/sortable.js']
+                        }
+                         ]);
+                }
+            }
+                        
       });
 
     $urlRouterProvider.otherwise('/index/main');
