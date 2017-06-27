0a02bf28fb1e9d255e10229530f983e34b9e4d9a
Chithralekha <chithra@altiussystems.com>
2017-06-27 15:44:35 -0400

    Taskboard

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('inspinia', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'ngRoute','oc.lazyLoad']);
+    .module('inspinia', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'ngRoute','oc.lazyLoad', 'ui.sortable']);
 
 })();
--- src/app/taskboard/taskboard.route.js
+++ src/app/taskboard/taskboard.route.js
@@ -13,17 +13,7 @@
         templateUrl: "app/taskboard/taskboard.html",
         data: { pageTitle: 'Taskboard' },
         controller: 'TaskboardController',
-        controllerAs: 'vm',        
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            name: 'ui.sortable',
-                            files: ['../../bower_components/angular-ui-sortable/sortable.js']
-                        }
-                         ]);
-                }
-            }
+        controllerAs: 'vm'
                         
       });
 
