04b6b3e696d6ffe6cb3e58d844d559f4a5b4675e
chithralekha\anupachitraprashanth <chithraprashanth@gmail.com>
2017-07-13 17:45:57 -0400

    Committ

--- src/app/core/dataservice.js
+++ src/app/core/dataservice.js
@@ -3,7 +3,10 @@
 
     angular
         .module('inspinia')
-        .factory('dataservice', dataservice);
+        .factory('dataservice', dataservice)
+        .value('taskDueStatusClassService', {
+            retrieveTaskDueStatusClass: retrieveTaskDueStatusClass
+        });
 
     /* @ngInject */
     function dataservice($http, $location, $q, exception, logger,config) {
@@ -16,8 +19,13 @@
 
         return service;
 
-        function getAvengers() {
-            return $http.get(config.baseURL + 'WorkingSets/1/Tasks?filterId=1')
+        function getAvengers(bcp, filterId) {
+            
+            var deferred = $q.defer();
+            
+            //var tasksPromise = getAllTasks(bcp, filterId);
+            
+            return $http.get(config.baseURL + 'WorkingSets/'+ bcp + '/Tasks?filterId=' + filterId)
                 .then(getAvengersComplete)
                 .catch(function(message) {
                     exception.catcher('XHR Failed for getAvengers')(message);
@@ -27,7 +35,45 @@
             function getAvengersComplete(data, status, headers, config) {
                 return data.data;
             }
+           // return deferred.promise;
         }
+        
+//        function getAllTasks(bcp, filterId) {
+//
+//            return $http.get(config.baseURL + 'WorkingSets/'+ bcp + '/Tasks?filterId=' + filterId)
+//            .then(sendResponseData)
+//            .catch(sendGetTasksError)
+//
+//        }
+//        
+//        function sendResponseData(response) {
+//
+//            return response.data;
+//
+//        }
+//
+//        function sendGetTasksError(response) {
+//
+//            return $q.reject('Error retrieving Tasks. (HTTP status: ' + response.status + ')');
+//
+//        }
 
+    }
+    
+    function retrieveTaskDueStatusClass(task) {
+        var dueStatusClass = null;
+        switch(task.dueStatus.status)
+        {
+            case 'Overdue' :
+                dueStatusClass = 'danger-element';
+                break;
+            case 'On Time' :
+                dueStatusClass  = 'success-element';
+                break;
+            case 'In Jeopardy' :
+                dueStatusClass = 'warning-element';
+                break;
+        }
+        return dueStatusClass;
     }
 })();
--- src/app/taskboard/taskboard.controller.js
+++ src/app/taskboard/taskboard.controller.js
@@ -1,49 +1,55 @@
 'use strict';
 
 angular.module('inspinia')
-    .controller('TaskboardController', function (dataservice, logger) {
+    .controller('TaskboardController', function (dataservice, taskDueStatusClassService, logger, $stateParams) {
     
-        var vm = this;
-
-        vm.userName = 'Example user';
-        vm.helloText = 'Taskboard';
-        vm.descriptionText = 'Taskboard';
-        
-        vm.taskDueStatusClass = function(task) {
-        switch(task.dueStatus.status)
-        {
-            case 'Overdue' :
-                return 'danger-element';
-                break;
-            case 'On Time' :
-                return 'success-element';
-                break;
-            case 'In Jeopardy' :
-                return 'warning-element';
-                break;
-        }
+    var vm = this;
+    
+    vm.userName = 'Example user';
+    vm.helloText = 'Taskboard';
+    vm.descriptionText = 'Taskboard';
+    vm.taskDueStatusClass = taskDueStatusClassService.retrieveTaskDueStatusClass;
+    
+    //Following function doesnot use the value service pointer.
+    //            function(task) {
+    //        switch(task.dueStatus.status)
+    //        {
+    //            case 'Overdue' :
+    //                return 'danger-element';
+    //                break;
+    //            case 'On Time' :
+    //                return 'success-element';
+    //                break;
+    //            case 'In Jeopardy' :
+    //                return 'warning-element';
+    //                break;
+    //        }
+    //    };
+    
+    vm.sortableOptions = {
+        connectWith: ".connectList"
     };
-        vm.sortableOptions = {
-            connectWith: ".connectList"
-        };
+    
+    activate();
+    
+    function activate() {
+        //Following comment is from the original sorce, CleanCode by John Papa
+        //            Using a resolver on all routes or dataservice.ready in every controller
+        //            var promises = [getAvengers()];
+        //            return dataservice.ready(promises).then(function(){
         
-        activate();
-
-
-        function activate() {
-//            Using a resolver on all routes or dataservice.ready in every controller
-//            var promises = [getAvengers()];
-//            return dataservice.ready(promises).then(function(){
-            return getAvengers().then(function () {
-                logger.info('Activated Avengers View');
+        return getAvengers($stateParams.bcp,$stateParams.filterText)
+                .then(function () {
+                logger.info('Activated Taskboard View');
             });
         }
-
-        function getAvengers() {
-            return dataservice.getAvengers().then(function (data) {
-                vm.todoList = data.taskInfos;
-                vm.inProgressList = data.taskInfos;
-                return vm.todoList;
-            });
-        }
-    });
+    
+    function getAvengers(bcp, filterId) {
+        return dataservice.getAvengers(bcp, filterId)
+            .then(function (data) {
+            vm.todoList = data.taskInfos;
+            vm.inProgressList = data.taskInfos;
+           return vm.todoList;
+        });
+    }
+});
