0a3c0ac830d3fd4fe08908d168d2b6efab864e01
Chithralekha <chithra@altiussystems.com>
2017-07-26 13:05:09 -0400

    UserRoles

--- src/app/core/constants.js
+++ src/app/core/constants.js
@@ -13,6 +13,25 @@
         //            'http://localhost:50443/',
         //            baseURL : 'http://localhost:3706/api/',
         //            authURL : 'http://ec2-54-148-177-100.us-west-2.compute.amazonaws.com/MagpieIdentity/',
-        oldTroutBaseURL : 'http://localhost:9092/Landing/index?n='
+        oldTroutBaseURL : 'http://localhost:9092/Landing/index?n='})
+        .constant('USER_ROLES', {
+        all : '*',
+        System : 'System',
+        NotificationService : 'NotificationService',
+        Debugger : 'Debugger',
+        Administrator : 'Administrator',
+        AccountManager : 'AccountManager',
+        UserManager : 'UserManager',
+        RaciTeamManager : 'RaciTeamManager',
+        BusinessProcessManager : 'BusinessProcessManager',
+        QlikUser : 'QlikUser',
+        TaskBoardUser : 'TaskBoardUser',
+        TaskBoardUserReadOnly : 'TaskBoardUserReadOnly',
+        ProgramDesigner : 'ProgramDesigner',
+        LogViewer : 'LogViewer',
+        DocumentRepositoryUser : 'DocumentRepositoryUser',
+        DocumentRepositoryUserReadOnly : 'DocumentRepositoryUserReadOnly',
+        IncidentResponseUser : 'IncidentResponseUser',
+        PolicyGeneratorUser : 'PolicyGeneratorUser'
     });
 })();
